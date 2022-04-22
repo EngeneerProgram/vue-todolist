635364acab2f959fa48d888ae1877a5818be1533
Rosario Garufi <saro.garufi1@gmail.com>
2022-04-22 15:38:57 +0200

    Initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+const app = new Vue(
+    {
+        el : "#app",
+        data:{
+            tasks:[
+                {
+                    text: "Fare la spesa",
+                    done: ""
+                },
+            ]
+        },
+        methods:{
+
+        }
+    }
+)
