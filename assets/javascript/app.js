a2735a4b502393151ebc3bbee8d42330f51c5eaa
Rosario Garufi <saro.garufi1@gmail.com>
2022-04-22 18:23:21 +0200

    implementazione esercizio

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,7 @@ const app = new Vue(
         el : "#app",
         
         data:{
-           
+           new_todo:"",
             
             tasks:[
                 {
@@ -31,16 +31,24 @@ const app = new Vue(
         methods:{
             delete_todo(i){
                 console.log("task da eliminare")
-                this.tasks.splice(this.tasks,1)
+                this.tasks.splice(i,1);
                 
             },
             new_task(){
                 console.log("nuova task")
+                console.log(this.new_todo);
                 
-            },
+                this.tasks.push({
+                    text: this.new_todo ,
+                    done: false})
+             },       
+                    
+                
+            
             task_done(i){
                 this.tasks[i].done = !this.tasks[i].done;
             }
         }
     }
+    
 )
