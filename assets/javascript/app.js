2b3f3e00f68fa04232cee98336a3bb9982322ffe
Rosario Garufi <saro.garufi1@gmail.com>
2022-04-26 09:29:11 +0200

    Consegna Esercizio

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,54 +1,62 @@
 const app = new Vue(
     {
         el : "#app",
-        
         data:{
-           new_todo:"",
-            
+            new_todo:"",
+
             tasks:[
                 {
                     text: "Fare la spesa",
-                    done: true
+                    done : true
                 },
-
                 {
-                    text: "Visita medica",
-                    done: false
+                    text: "Andare dal dentista",
+                    done : false
                 },
-
+                
                 {
-                    text: "Pulire casa",
-                    done: false
+                    text: "Visita veterinario",
+                    done : false
                 },
-
-                {
-                    text: "Cucinare",
-                    done: true
-                },
-            ],
-
+            ]
         },
         methods:{
+            task_done(i){
+               console.log(this.tasks)
+               this.tasks[i].done = !this.tasks[i].done;
+            },
             delete_todo(i){
-                console.log("task da eliminare")
-                this.tasks.splice(i,1);
-                
+                this.tasks.splice(i, 1);
             },
             new_task(){
-                console.log("nuova task")
-                console.log(this.new_todo);
                 
+                console.log("nuova task");
                 this.tasks.push({
-                    text: this.new_todo ,
-                    done: false})
-             },       
+                    text:this.new_todo,
+                    done:false
                     
+                })
+                this.new_todo = ""
                 
-            
-            task_done(i){
-                this.tasks[i].done = !this.tasks[i].done;
             }
         }
     }
-    
 )
+
+
+//data
+
+let date = new Date();
+const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+let day = days[date.getDay()];
+console.log(day)
+
+document.getElementById("day").innerHTML = day;
+
+//date corrente
+
+let gg, mm, aaaa;
+gg = date.getDate() + "/";
+mm = date.getMonth() + 1 + "/";
+aaaa=date.getFullYear();
+document.getElementById("full_data").innerHTML = gg + mm + aaaa;
